eb700cb833d9386b35220f537f189ff12d006ca8
githublearning <sai.sreekar150@gmail.com>
2026-02-10 21:04:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const params = new URLSearchParams(window.location.search);
+const from = params.get("from") || "Someone";
+const to = params.get("to") || "You";
+
+document.getElementById("question").innerText =
+  `${to}, will you be my Valentine?`;
+
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const hint = document.getElementById("hint");
+
+noBtn.addEventListener("mouseover", () => {
+  noBtn.style.position = "absolute";
+  noBtn.style.left = Math.random() * 80 + "%";
+  noBtn.style.top = Math.random() * 80 + "%";
+  hint.innerText = `"No" seems a bit shy 😈`;
+});
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div class="card">
+      <img src="https://i.imgur.com/Q9BGTuy.png" class="cat">
+      <h1>Yay! 💖</h1>
+      <p>${from} will be so happy!</p>
+    </div>
+  `;
+});
